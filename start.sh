7192f61069efb2162c9952df370dcd1743d77a56
Heaust-ops <heaust.azure@gmail.com>
2021-08-31 17:25:30 +0530

    updated build and start with absolute paths

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-[ ! -d "env" ] && sh ./build.sh
-source env/bin/activate
-python main.py
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+[ ! -d "$SCRIPT_DIR/env" ] && sh $SCRIPT_DIR/build.sh
+source $SCRIPT_DIR/env/bin/activate
+python $SCRIPT_DIR/main.py
